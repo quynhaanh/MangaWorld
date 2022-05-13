0e2ac638ec46a7c2a74c4918a7a4d764c40ed25c
Tuan Anh <tuananhxvt@gmail.com>
2022-05-13 17:36:33 +0700

    CRUD chapter

--- app/src/main/java/com/example/mangaworld/activity/AdminActivity.java
+++ app/src/main/java/com/example/mangaworld/activity/AdminActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.mangaworld.R;
 
@@ -32,7 +31,7 @@ public class AdminActivity extends AppCompatActivity {
 
     public void gotoChapter(View view) {
 
-        startActivity(new Intent(this, CRUDChapter.class));
+        startActivity(new Intent(this, CRUDChapterActivity.class));
     }
 
     public void gotoChart(View view) {
--- app/src/main/java/com/example/mangaworld/activity/CRUDAuthorActivity.java
+++ app/src/main/java/com/example/mangaworld/activity/CRUDAuthorActivity.java
@@ -94,6 +94,7 @@ public class CRUDAuthorActivity extends AppCompatActivity {
 
                 updateFlag = false;
 
+
             }
         });
     }
