c61ef2df3b7aaeccdecbf6db70a710ddf8bc4a5e
n18dccn106@student.ptithcm.edu.vn <n18dccn106@student.ptithcm.edu.vn>
2022-05-13 17:50:03 +0700

    Modified Your Novel Activity

--- app/src/main/java/com/example/mangaworld/activity/YourNovelActivity.java
+++ app/src/main/java/com/example/mangaworld/activity/YourNovelActivity.java
@@ -2,22 +2,30 @@ package com.example.mangaworld.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.mangaworld.R;
+import com.example.mangaworld.adapter.NovelItemAdapter;
 import com.example.mangaworld.controller.IVolleyCallback;
 import com.example.mangaworld.controller.NovelController;
+import com.example.mangaworld.model.NovelModel;
+
+import java.util.ArrayList;
 
 public class YourNovelActivity extends AppCompatActivity {
 
     Button btnYourNovelToAdding;
     ListView lvYourNovel;
 
-    NovelController novelController;
-
     String url = LoadActivity.url;
+
+    NovelController novelController;
+    ArrayList<NovelModel> novelData = new ArrayList<>();
+    NovelItemAdapter novelItemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +33,19 @@ public class YourNovelActivity extends AppCompatActivity {
         setContentView(R.layout.activity_your_novel);
         setControl();
         setEvent();
+        refreshListYourNovel();
     }
 
     private void setEvent() {
+        lvYourNovel.setAdapter(novelItemAdapter);
+        btnYourNovelToAdding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(getActivity, YourNovelAddActivity.class);
+//                startActivity(intent);
+                startActivity(new Intent(YourNovelActivity.this, YourNovelAddActivity.class));
+            }
+        });
     }
 
     private void setControl() {
@@ -35,7 +53,7 @@ public class YourNovelActivity extends AppCompatActivity {
         lvYourNovel = findViewById(R.id.lvYourNovel);
 
         novelController = new NovelController(url, this);
-
+        novelItemAdapter = new NovelItemAdapter(YourNovelActivity.this, R.layout.layout_item_novel, novelData, url);
     }
 
     public void refreshListYourNovel()
@@ -43,7 +61,9 @@ public class YourNovelActivity extends AppCompatActivity {
         novelController.getNovel(new IVolleyCallback() {
             @Override
             public void onSuccess(String result) {
-
+                novelData.clear();
+                novelData.addAll(novelController.convertJSONData(result));
+                novelItemAdapter.notifyDataSetChanged();
             }
         });
     }
