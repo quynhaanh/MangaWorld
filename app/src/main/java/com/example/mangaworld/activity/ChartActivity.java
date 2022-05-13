290c1768a0749085f436864f4cfbcabca7ec3dd3
Đăng Nguyễn Hải <goingmyway243@gmail.com>
2022-05-14 00:07:35 +0700

    Dang tmp commit2

--- app/src/main/java/com/example/mangaworld/activity/ChartActivity.java
+++ app/src/main/java/com/example/mangaworld/activity/ChartActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -16,24 +17,46 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.mangaworld.R;
 import com.example.mangaworld.controller.IVolleyCallback;
 import com.example.mangaworld.controller.NovelController;
+import com.example.mangaworld.model.ChapterModel;
 import com.example.mangaworld.model.NovelModel;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChartActivity extends AppCompatActivity {
-    Button btnChoose, btnUpload;
-    ImageView imageView;
-    EditText edName;
-    Bitmap bitmap;
+    PieChart pieChart;
+    String pieChartType = "count";
+    String barChartType = "sum";
+    int[] colorTemplate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,89 +68,80 @@ public class ChartActivity extends AppCompatActivity {
     }
 
     private void setEvent() {
-        btnChoose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                selectImage();
-
-                NovelController controller = new NovelController(LoadActivity.url,ChartActivity.this);
-                controller.getNovelByIDUser(MainActivity.loggedUser.getId(), new IVolleyCallback() {
-                    @Override
-                    public void onSuccess(String result) {
-                        Log.d("test", controller.convertJSONData(result).get(0).getDescription());
-                    }
-                });
-            }
-        });
-
-        btnUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                uploadImage();
-            }
-        });
+        initPieChart();
     }
 
     private void setControl() {
-        btnChoose = findViewById(R.id.btnChooseImage);
-        btnUpload = findViewById(R.id.btnUploadImage);
-        imageView = findViewById(R.id.ivUploadImage);
-        edName = findViewById(R.id.edImageName);
+        pieChart = findViewById(R.id.pieChart);
+        colorTemplate = new int[]{R.color.colorLightRed, R.color.colorSkyBlue, R.color.green_color,
+                R.color.teal_200, R.color.teal_700, R.color.purple_200,
+                R.color.purple_500, R.color.purple_700, R.color.line_color};
     }
 
-    private void selectImage() {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent, 1);
-    }
+    private void initPieChart() {
+        String urlAPI = LoadActivity.url + "/api/truyenchu/api_chart.php";
 
-    private void uploadImage() {
-        NovelModel model = new NovelModel();
-        model.setId(1);
-        model.setTitle("Test");
-        model.setIdAuthor(1);
-        model.setCover(edName.getText().toString());
-        model.setDescription("Test Upload Cover");
-//        model.setDatePost(getDateString()); Không cần thiết
-        model.setIdUser(MainActivity.loggedUser.getId());
-        model.setCoverImageData(imageToString(bitmap));
-
-        NovelController controller = new NovelController(LoadActivity.url, this);
-        controller.insertNovel(model, new IVolleyCallback() {
+        StringRequest request = new StringRequest(Request.Method.POST, urlAPI, new Response.Listener<String>() {
             @Override
-            public void onSuccess(String result) {
+            public void onResponse(String response) {
+                Log.d("test", response);
+                handlePieChartData(response);
             }
-        });
-    }
-
-    private String getDateString() {
-        Date date = Calendar.getInstance().getTime();
-        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        return formatter.format(date);
-    }
-
-    private String imageToString(Bitmap bitmap) {
-        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteOut);
-        byte[] imgBytes = byteOut.toByteArray();
-        return Base64.encodeToString(imgBytes, Base64.DEFAULT);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
-            Uri path = data.getData();
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), path);
-                imageView.setImageBitmap(bitmap);
-                imageView.setVisibility(View.VISIBLE);
-                edName.setVisibility(View.VISIBLE);
-            } catch (IOException e) {
-                e.printStackTrace();
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
             }
+        }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("Type", pieChartType);
+
+                return params;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        queue.add(request);
+    }
+
+    private void handlePieChartData(String result) {
+        ArrayList<PieEntry> dataEntry = new ArrayList<>();
+
+        try {
+            JSONObject jsonObject = new JSONObject(result);
+            JSONArray jsonArray = jsonObject.getJSONArray("statistics");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject object = jsonArray.getJSONObject(i);
+
+                int value = object.getInt("Count");
+                String genreName = object.getString("Genre_Name");
+
+                dataEntry.add(new PieEntry(value, genreName));
+            }
+
+        } catch (JSONException ex) {
+            ex.printStackTrace();
         }
+
+        PieDataSet dataSet = new PieDataSet(dataEntry, "");
+        dataSet.setColors(colorTemplate,getApplicationContext());
+        dataSet.setValueTextColor(Color.BLACK);
+        dataSet.setValueTextSize(15f);
+
+        PieData pieData = new PieData(dataSet);
+        pieData.setValueFormatter(new PercentFormatter(pieChart));
+
+        pieChart.setEntryLabelTextSize(10f);
+        pieChart.setData(pieData);
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.getLegend().setEnabled(false);
+        pieChart.setCenterTextSize(15f);
+        pieChart.setCenterText("Tỉ lệ số lượng truyện theo từng thể loại");
+        pieChart.animateXY(1000, 1000);
     }
 }
