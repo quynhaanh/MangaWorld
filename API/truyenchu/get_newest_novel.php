63c01a044a8c2e3fdf1a7b0a3f804aef09b5c898
Đăng Nguyễn Hải <goingmyway243@gmail.com>
2022-06-09 23:56:23 +0700

    Fix API get_newest_novel.php

--- API/truyenchu/get_newest_novel.php
+++ API/truyenchu/get_newest_novel.php
@@ -2,7 +2,7 @@
 require "connect.php";
 
 $query = "SELECT DISTINCT n.ID, n.Title, ID_Author, Description, Cover, n.Date_Post, View, ID_User 
-FROM novel n, (SELECT ID_Novel, Date_Post FROM chapter ORDER BY Date_Post DESC) c WHERE n.ID = c.ID_Novel
+FROM novel n, (SELECT DISTINCT ID_Novel, Date_Post FROM chapter ORDER BY Date_Post DESC) c WHERE n.ID = c.ID_Novel
 ORDER BY c.Date_Post DESC";
 $result = mysqli_query($connect, $query);
 $temp_array = array();
